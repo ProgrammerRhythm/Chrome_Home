163a8031fb46704211b94d3fb5ab4ba5977354a2
Rhythm Munshi <programmingwithrm@gmail.com>
2022-04-27 18:30:09 +0600

    Got some basic changes

--- app.js
+++ app.js
@@ -3,24 +3,42 @@ var time = today.getHours();
 let wish = "";
 if(time>=21 && time>= 18){
     wish = "Night";
+    document.body.style.backgroundImage = "url('8.jpeg')";
 }
-else if(time>= 18 && time<=21){
+else if(time>= 19 && time<=21){
     wish = "Evening";
+    document.body.style.backgroundImage = "url('7.jpeg')";
+
 }
-else if(time>= 13 && time<=18){
+else if(time>= 18 && time<=19){
+    document.body.style.backgroundImage = "url('6.jpeg')";
     wish = "Afternoon";
 }
-else if(time>= 7 && time<=13){
+else if(time>= 15 && time<=18){
+    document.body.style.backgroundImage = "url('5.jpeg')";
+    wish = "On going Afternoon";
+}
+else if(time>= 12 && time<=15){
+    document.body.style.backgroundImage = "url('4.jpeg')";
     wish = "Morning";
 }
-else if(time>= 0 && time<=7){
+else if(time>= 8 && time<=12){
+    document.body.style.backgroundImage = "url('3.jpeg')";
+    wish = "Morning";
+}
+else if(time>= 5 && time<=8){
+    document.body.style.backgroundImage = "url('2.jpeg')";
+    wish = "Early Morning";
+}
+else if(time>= 0 && time<=5){
+    document.body.style.backgroundImage = "url('1.jpeg')";
     wish = "Early Morning";
 }
 document.getElementById("wish").innerHTML = wish;
-var value = Math.random()*100;
-value = Math.round(value);
-var url = `https://picsum.photos/2000/1900?random=${value}`
-document.body.style.backgroundImage = `url(${url})`;
+// var value = Math.random()*100;
+// value = Math.round(value);
+// var url = `https://picsum.photos/2000/1900?random=${value}`
+// document.body.style.backgroundImage = `url(${url})`;
 
 
 function clock() {
